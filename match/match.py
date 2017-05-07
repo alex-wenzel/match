a5fbc034f95a6815b7967965790a26a83d964134
KwatME <kwat.medetgul.ernar@gmail.com>
2017-05-07 15:43:50 -0700

    Rap

--- match/match.py
+++ match/match.py
@@ -1,16 +1,3 @@
-"""
-Computational Cancer Analysis Library
-
-Authors:
-    Huwate (Kwat) Yeerna (Medetgul-Ernar)
-        kwat.medetgul.ernar@gmail.com
-        Computational Cancer Analysis Laboratory, UCSD Moore's Cancer Center
-
-    Pablo Tamayo
-        ptamayo@ucsd.edu
-        Computational Cancer Analysis Laboratory, UCSD Moore's Cancer Center
-"""
-
 from math import ceil, sqrt
 from os.path import join
 
