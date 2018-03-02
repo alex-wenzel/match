6b830f8c9d88de4d319eb9b316707124686e14a9
KwatME <kwatme8@gmail.com>
2018-03-01 20:03:25 -0800

    Colors

--- match/plot_match_panel.py
+++ match/plot_match_panel.py
@@ -1,11 +1,11 @@
 from matplotlib.colorbar import ColorbarBase, make_axes
+from matplotlib.colors import Normalize
 from matplotlib.gridspec import GridSpec
 from matplotlib.pyplot import figure, subplot
+from numpy import nanmax, nanmean, nanmin
 from pandas import DataFrame, Series
 from seaborn import heatmap
 
-from .nd_array.nd_array.clip_nd_array_by_standard_deviation import \
-    clip_nd_array_by_standard_deviation
 from .nd_array.nd_array.normalize_1d_array import normalize_1d_array
 from .nd_array.nd_array.normalize_2d_array import normalize_2d_array
 from .plot.plot.decorate_ax import decorate_ax
@@ -36,46 +36,58 @@ def plot_match_panel(target, features, target_type, features_type, target_ax,
     Returns:
     """
 
-    if target_type == 'continuous':
-        target_cmap = CMAP_CONTINUOUS_BWR2
+    max_std = 3
 
+    if target_type == 'continuous':
         target = Series(
-            normalize_1d_array(
-                clip_nd_array_by_standard_deviation(target, 3), '0-1'),
+            normalize_1d_array(target.values, method='-0-').clip(
+                -max_std, max_std),
             name=target.name,
             index=target.index)
 
-    elif target_type == 'categorical':
-        target_cmap = CMAP_CATEGORICAL
-
-    elif target_type == 'binary':
-        target_cmap = CMAP_BINARY_WB
+        target_min = nanmin(target.values)
+        target_max = nanmax(target.values)
+        target_cmap = CMAP_CONTINUOUS_BWR2
 
     else:
-        raise ValueError('Unknown target_type: {}.'.format(target_type))
+        target_min = 0
+        target_max = target.unique().size
+
+        if target_type == 'categorical':
+            target_cmap = CMAP_CATEGORICAL
+
+        elif target_type == 'binary':
+            target_cmap = CMAP_BINARY_WB
+
+        else:
+            raise ValueError('Unknown target_type: {}.'.format(target_type))
 
     if features_type == 'continuous':
-        features_cmap = CMAP_CONTINUOUS_BWR2
-
         features = DataFrame(
-            normalize_2d_array(
-                features.apply(
-                    clip_nd_array_by_standard_deviation, axis=1, args=(3, )),
-                '0-1', 1),
+            normalize_2d_array(features.values, method='-0-', axis=1).clip(
+                -max_std, max_std),
             index=features.index,
             columns=features.columns)
 
-    elif features_type == 'categorical':
-        features_cmap = CMAP_CATEGORICAL
-
-    elif features_type == 'binary':
-        features_cmap = CMAP_BINARY_WB
+        features_min = nanmin(features)
+        features_max = nanmax(features)
+        features_cmap = CMAP_CONTINUOUS_BWR2
 
     else:
-        raise ValueError('Unknown features_type: {}.'.format(features_type))
+        features_min = 0
+        features_max = features.unstack().unique().size
+
+        if features_type == 'categorical':
+            features_cmap = CMAP_CATEGORICAL
+
+        elif features_type == 'binary':
+            features_cmap = CMAP_BINARY_WB
+
+        else:
+            raise ValueError(
+                'Unknown features_type: {}.'.format(features_type))
 
     if target_ax is None or features_ax is None:
-
         figure(figsize=(min(pow(features.shape[1], 1.8), FIGURE_SIZE[1]),
                         features.shape[0]))
 
@@ -90,8 +102,14 @@ def plot_match_panel(target, features, target_type, features_type, target_ax,
                 colorbar_ax,
                 location='bottom',
                 fraction=0.2,
-                cmap=features_cmap)
+                cmap=features_cmap,
+                norm=Normalize(vmin=features_min, vmax=features_max),
+                ticks=(
+                    features_min,
+                    nanmean(features),
+                    features_max, ))
             ColorbarBase(colorbar_ax_, **kwargs)
+
             decorate_ax(colorbar_ax_)
 
     if len(target_xticklabels) and len(target_xticklabels) != target.size:
@@ -101,6 +119,8 @@ def plot_match_panel(target, features, target_type, features_type, target_ax,
         DataFrame(target).T,
         ax=target_ax,
         cmap=target_cmap,
+        vmin=target_min,
+        vmax=target_max,
         xticklabels=target_xticklabels,
         yticklabels=(target.name, ),
         cbar=False)
@@ -133,6 +153,8 @@ def plot_match_panel(target, features, target_type, features_type, target_ax,
         features,
         ax=features_ax,
         cmap=features_cmap,
+        vmin=features_min,
+        vmax=features_max,
         xticklabels=plot_column_names,
         cbar=False)
 
