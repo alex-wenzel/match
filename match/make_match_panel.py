1f096ed6412dd224e928aea4861d4fe4f5c06833
Kwat <kwatme8@gmail.com>
2018-03-03 15:03:08 -0800

    Update make_match_panel.py

--- match/make_match_panel.py
+++ match/make_match_panel.py
@@ -58,7 +58,7 @@ def make_match_panel(target,
         features_type (str): 'continuous' | 'categorical' | 'binary'
         plot_max_std (float):
         title (str):
-        target_xticklabels (iterable): same length as target
+        target_xticklabels (iterable): (n_sample, )
         max_ytick_size (int):
         plot_column_names (bool):
         file_path_prefix (str):
