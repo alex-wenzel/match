802cc66a4d9f4c44e6a419d03809b9003c50f80c
KwatME <kwatme8@gmail.com>
2018-05-15 21:26:13 -0700

    all instead of any

--- match/_check_features_index.py
+++ match/_check_features_index.py
@@ -1,5 +1,5 @@
 def _check_features_index(features):
 
-    if any(str(i).isdigit() for i in features.index):
+    if all(str(i).isdigit() for i in features.index):
 
         raise ValueError('Use only non-digit features index.')
